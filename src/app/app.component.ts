c9c2e4233e1e0c7b7e8e7e16e2a8403c7b687cdd
aluckya100@gmail.com <aluckya100@gmail.com>
2022-07-25 23:55:30 -0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CartService } from "./services/cart.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css'],
+
 })
+
 export class AppComponent {
-  title = 'firstproject';
+    constructor(private cartService: CartService){
+
+    }
+    getCart(){
+        return this.cartService.get();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
+import { BooksModule } from "./books/books.module";
+import { BooksService } from "./books/books.service";
+import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, CartComponent, ],
+    imports: [BrowserModule, AuthModule, BooksModule, AppRoutingModule],
+
+    bootstrap:[AppComponent]
 })
-export class AppModule { }
+
+export class AppModule{
+
+}
